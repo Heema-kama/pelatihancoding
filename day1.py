b6ad774251b787483e569e9cb5ddb798cdfc4980
Heema-kama <167331513+Heema-kama@users.noreply.github.com>
2024-09-07 11:06:27 +0700

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,22 @@
+# a = "apel" # "a" adalah variabel dan "apel" adalah data
+# print(a)
+
+# buah = "jeruk" #ini adalah tipedata 'string'
+# harga = 15000 #ini tipedata 'integer'
+# berat = 1.5 #ini adalah tipedata 'float'
+# beli = True #ini adalah tipedata 'boolean'
+
+# print(buah)
+# print(harga)
+# print(berat)
+# print(beli)
+
+print("halo perkenalkan nama aku Bot!")
+print("halo, bot!")
+nama = input("nama, kamu siapa?")
+print (f"namaku {nama}")
+print (f"halo, {nama}. salam kenal!")
+print("kamu sedang sibuk apa sekarang, {nama?}") 
+kegiatan = input("kesibukanmu?")
+print (f"aku lagi sibuk {kegiatan} sekarang.")
+print (f"oke, semogan{kegiatan} berhasil")
